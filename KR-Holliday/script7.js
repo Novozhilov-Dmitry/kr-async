d069cf5df0b938ac065a2f2a7e42ed769a42cc05
Novozhilov Dmitry <novozhilov200320@gmail.com>
2023-02-20 23:48:16 +0300

    ver 1.1

--- KR-Holliday/script7.js
+++ KR-Holliday/script7.js
@@ -4,9 +4,13 @@ async function krData(god, strana_cod){
         const strana_cod1 = strana_cod.value;
         const response = await fetch(`https://date.nager.at/api/v3/PublicHolidays/${god1}/${strana_cod1}`)
         let data = await response.json();
-        let sorted = data.sort((a, b) => a.localName > b.localName ? 1 : -1);
-        console.log(sorted);
+        await getValue(data);
     }catch(err){
-        console.log('Errrorrrr >>> , err');
-    }
+    console.log('Введён неверный код страны.' , err);
+}
+}
+function getValue(data) {
+
+    let sorted = data.sort((a, b) => a.localName > b.localName ? 1 : -1);
+    console.log(sorted);
 }
